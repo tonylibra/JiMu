8ed92c8f6c250fea6bc6182a3dd2968b385159d4
jhu <jingweihu.usc@gmail.com>
2021-12-19 10:55:10 -0800

    [Dec 19][Jingwei] Add Compose support and examples of two fragment

--- app/src/main/java/com/application/jimu/ui/home/HomeFragment.kt
+++ app/src/main/java/com/application/jimu/ui/home/HomeFragment.kt
@@ -1,22 +1,49 @@
 package com.application.jimu.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.application.jimu.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import com.application.jimu.databinding.FragmentComposeBinding
+import com.application.jimu.ui.theme.JimuTheme
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
+    lateinit var binding: FragmentComposeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentComposeBinding.inflate(inflater, container, false)
+        return binding.composeView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                JimuTheme {
+                    Row(
+                        modifier = Modifier.fillMaxHeight(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "Hello")
+                        Text(text = "HomeFragment")
+                    }
+                }
+            }
+        }
     }
 }
